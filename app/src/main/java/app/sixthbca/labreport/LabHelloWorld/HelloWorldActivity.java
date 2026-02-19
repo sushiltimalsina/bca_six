73b249cc8ee8cd6502ac23a7372c94d0e38318d9
sushiltimalsina <sushiltimalsina06@gmail.com>
2026-02-19 14:33:43 +0545

    Lab HelloWorld Created

---
+++ app/src/main/java/app/sixthbca/labreport/LabHelloWorld/HelloWorldActivity.java
@@ -0,0 +1,13 @@
+package app.sixthbca.labreport.LabHelloWorld;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import app.sixthbca.R;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_hello_world);
+    }
+}
